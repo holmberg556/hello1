674d8e3695d498a3aa2f6ab51cc7efee5785d187
Johan Holmberg <holmberg556@gmail.com>
2019-11-13 08:37:44 +0100

    initial version

---
+++ src/hello.cpp
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+#include "world.h"
+
+int
+main(int argc, char* argv[])
+{
+  printf("Hello %s!\n", world);
+  return 0;
+}
---
+++ src/subdir/world.c
@@ -0,0 +1,5 @@
+
+#include "world.h"
+
+const char world[] = "World";
+
